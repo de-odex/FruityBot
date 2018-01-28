2d7e7e62320f7c3294478c9b4bc007d95217cca8
de-odex <justinfblover@gmail.com>
2018-01-28 15:28:37 +0800

    v3.1: Rewrote using twisted-irc hybrid (irc's reactor had issues)
    Signed-off-by: Justin Kyle Ramos <justinfblover@gmail.com>

--- bot.py
+++ bot.py
@@ -1,147 +1,185 @@
-import irc.bot
-import irc.strings
-from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
-import logging
 import logging.config
 
-#mine
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol, threads
+
+# mine
 import os
 import importlib
-
-import time
-import sqlite3
-import utils
+import datetime
 import pathlib
-import slider
+import box
+import utils
+import external.jaraco_irc_to_twisted as jaraco_irc_to_twisted
 
 logging.config.fileConfig('logging.conf')
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
-class FruityBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, channel, port=6667, test=False):
+class FruityBot(irc.IRCClient):
+    try:
+        nickname = utils.Config("config.json").config.main.nick
+        password = utils.Config("config.json").config.osu.irc
+    except FileNotFoundError:
+        nickname = utils.Config("config.json.template").config.main.nick
+        password = utils.Config("config.json.template").config.osu.irc
+    lineRate = 1
+    heartbeatInterval = 64
+
+    def __init__(self, first_time, users, channel=None, test=False):
+        logger.debug("Trying nickname " + self.nickname)
         self.test = test
-        self.nick_iteration = 0
-
-        self.connection.set_rate_limit(1)
+        self.channel = channel
 
         try:
             self.Config = utils.Config("config.json")
         except FileNotFoundError:
             self.Config = utils.Config("config.json.template")
 
-        self.userdb = sqlite3.connect('userpref.db')
-        self.upcur = self.userdb.cursor()
-        try:
-            self.upcur.execute("CREATE TABLE IF NOT EXISTS userdb (user INT PRIMARY KEY, mode INT, info INT)")
-        except self.userdb.Error:
-            logger.error("Failed to create the user database!")
-
-        # test for info column
-        self.results = self.upcur.execute("PRAGMA table_info(userdb)").fetchall()
-        self.results2 = []
-        for i in self.results:
-            self.results2.append(i[1])
-        if "info" not in self.results2:
-            self.upcur.execute("ALTER TABLE userdb ADD info INT")
-
-        irc.bot.SingleServerIRCBot.__init__(self, [(self.Config.config.main.server, port)],
-                                            self.Config.config.main.nick, self.Config.config.main.nick)
-        self.channel = channel
-
         self.UPDATE_MSG = self.Config.config.main.update_msg
         self.FIRST_TIME_MSG = self.Config.config.main.first_time_msg
 
-        self.users = {}
-        self.libdir = pathlib.Path("./osulib").absolute()
+        self.user_pref = utils.Utils.create_db("./userpref.db", "userpref")
+        self.start_time = first_time
 
-        if not self.libdir.exists():
-            os.makedirs(self.libdir)
-            self.osu_library = slider.library.Library.create_db(self.libdir)
-            logger.info("Created osu! library")
-        else:
-            self.osu_library = slider.library.Library(self.libdir)
-        self.osu_api_client = slider.client.Client(self.osu_library, self.Config.config.osu.api)
+        self.users = users
 
-        self.start_time = time.time()
-
-        self.Commands = utils.Commands(self.Config)
+        self.Commands = utils.Commands(self, self.Config)
         self.command_funcs = [func for func in dir(utils.Commands) if callable(getattr(utils.Commands, func))
                               and not func.startswith("_")]
 
-    def on_nicknameinuse(self, c, e):
-        self.nick_iteration += 1
-        c.nick(c.get_nickname() + ("_" * self.nick_iteration))
-        logger.debug("Now using " + c.get_nickname() + ("_" * self.nick_iteration))
+    def irc_ERR_NICKNAMEINUSE(self, prefix, params):
+        logger.warning("Nick error! Someone of " + self.nickname + " nickname already exists")
+        self.nickname = self.nickname + "_"
+        self.setNick(self.nickname)
+        logger.debug("Now using " + self.nickname)
 
-    def on_welcome(self, c, e):
-        logger.info("Bot started as " + c.get_nickname())
-        c.join(self.channel)
+    def signedOn(self):
+        logger.info("Bot started as " + self.nickname + " at " + self.Config.config.main.server)
+        self.startHeartbeat()
+        if self.channel is not None and self.Config.config.main.server != "cho.ppy.sh" or \
+           self.Config.config.main.server != "irc.ppy.sh":
+            self.join(self.channel)
         if self.test:
-            self.die()
+            self.quit()
 
-    def on_privmsg(self, c, e):
-        logger.info(e.source.nick + ": " + e.arguments[0])
-        self.do_command(e, e.arguments[0])
+    def joined(self, channel):
+        logger.info("I have joined " + channel)
 
-    def on_action(self, c, e):
-        logger.info("* " + e.source.nick + " " + e.arguments[0])
-        self.Commands.np(self, c, e)
+    def privmsg(self, userhost, channel, msg):
+        user = userhost.split('!', 1)[0]
+        logger.info(user + ": " + msg)
+        if msg[0] == self.Config.config.main.prefix:
+            try:
+                threads.deferToThread(self.message_to_commands, userhost, msg)
+            except Exception:
+                logger.exception("Deferred Exception")
+                self.msg(user, "Falling back to single-thread...")
+                self.message_to_commands(userhost, msg)
 
-    def on_pubmsg(self, c, e):
-        if e.source.nick == self.Config.config.main.owner:
-            self.do_command(e, e.arguments[0])
+    def action(self, userhost, channel, data):
+        user = userhost.split('!', 1)[0]
+        logger.info("* " + user + " " + data)
+        try:
+            threads.deferToThread(self.message_to_commands, userhost, "!np " + data)
+        except Exception:
+            logger.exception("Deferred Exception")
+            self.msg(user, "Falling back to single-thread...")
+            self.message_to_commands(userhost, "!np " + data)
 
-    def do_command(self, e, cmd):
-        c = self.connection
+    def message_to_commands(self, userhost, msg):
+        commands = msg.split(self.Config.config.main.prefix)[1].split(";")
+        for msgs in commands:
+            msgs = msgs.strip()
+            self.do_command(userhost, msgs)
 
-        # check if user in database
-        in_db = utils.Utils.check_user_in_db(e.source.nick, 'userpref.db', "ftm")
-        if not in_db:
-            c.privmsg(e.source.nick, self.FIRST_TIME_MSG)
-        in_db = utils.Utils.check_user_in_db(e.source.nick, 'userpref.db', "um")
-        if not in_db:
-            c.privmsg(e.source.nick, self.UPDATE_MSG)
+    def do_command(self, userhost, msg):
+        cmd = msg.split()[0]
+        e = box.Box({'source': jaraco_irc_to_twisted.NickMask(userhost), 'arguments': [msg]})
 
-        if cmd == self.Config.config.main.prefix + "reload":
+        if cmd == "reload":
+            logger.debug("Command incurred: " + cmd)
             if e.source.nick == self.Config.config.main.owner:
-                c.privmsg(e.source.nick, "Attempting a reload...")
+                self.msg(e.source.nick, "Attempting a reload...")
                 try:
                     importlib.reload(utils)
+                    self.user_pref = utils.Utils.create_db("./userpref.db", "userpref")
                     self.Config = utils.Config("config.json")
-                    self.Commands = utils.Commands(self.Config)
+                    self.Commands = utils.Commands(self, self.Config)
                     self.UPDATE_MSG = self.Config.config.main.update_msg
                     self.FIRST_TIME_MSG = self.Config.config.main.first_time_msg
-                    self.command_funcs = [func for func in dir(utils.Commands) if callable(getattr(utils.Commands, func))
-                                          and not func.startswith("_")]
-                    self.osu_library = slider.library.Library.create_db(self.libdir)
-                    self.osu_api_client = slider.client.Client(self.osu_library, self.Config.config.osu.api)
-                    c.privmsg(e.source.nick, "Reload successful!")
+                    self.command_funcs = [func for func in dir(utils.Commands) if callable(
+                        getattr(utils.Commands, func)) and not func.startswith("_")]
+                    self.msg(e.source.nick, "Reload successful!")
                 except:
                     logger.exception("Reload Exception")
-                    c.privmsg(e.source.nick, "Reload failed! Killing bot due to possible errors.")
-                    self.die()
-            else:
-                c.privmsg(e.source.nick, "You do not have the permissions to run this command!")
+                    self.msg(e.source.nick, "Reload failed! Killing bot due to possible errors.")
+                    self.quit()
         else:
-            command_incurred = False
             for i in self.command_funcs:
-                if cmd.split()[0] == self.Config.config.main.prefix+i and i[:4] != "cmd_" or \
-                   cmd.split()[0] == self.Config.config.main.prefix+i[4:] and i[:4] == "cmd_":
-                    command_incurred = True
+                if (cmd.split()[0] == i and i[:4] != "cmd_") or (cmd.split()[0] == i[4:] and i[:4] == "cmd_"):
+                    logger.debug("Command incurred: " + cmd)
+                    # check if user in database
+                    in_db = utils.Utils.check_user_in_db(e.source, self, "ftm")
+                    if not in_db:
+                        self.msg(e.source.nick, self.FIRST_TIME_MSG)
+                    in_db = utils.Utils.check_user_in_db(e.source, self, "um")
+                    if not in_db:
+                        self.msg(e.source.nick, self.UPDATE_MSG)
+
                     func = getattr(utils.Commands, i)
-                    func(self.Commands, self, c, e)
-            if not command_incurred and cmd[:1] == self.Config.config.main.prefix:
-                c.privmsg(e.source.nick, "Invalid command. " + self.Config.config.main.prefix + "h for help.")
+                    func(self.Commands, self, e)
+                    break
+            else:
+                self.msg(e.source.nick, "Invalid command: " + cmd + ". " +
+                         self.Config.config.main.prefix + "h for help.")
+
+
+class BotFactory(protocol.ReconnectingClientFactory):
+    """A factory for Bots.
+
+    A new protocol instance will be created each time we connect to the server.
+    """
+
+    maxDelay = 5
+    initialDelay = 5
+
+    def __init__(self, channel=None):
+        self.first_time = datetime.datetime.now()
+        self.channel = channel
+        self.users = {}
+
+    def buildProtocol(self, addr):
+        p = FruityBot(self.first_time, self.users, self.channel)
+        p.factory = self
+        return p
+
+    def clientConnectionLost(self, connector, reason):
+        logger.warning('Lost connection.  Reason:' + str(reason).replace('\n', '').replace('\r', ''))
+        protocol.ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        logger.warning('Connection failed.  Reason:' + str(reason).replace('\n', '').replace('\r', ''))
+        protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
 
 def main():
+    if not pathlib.Path("./log/").exists():
+        os.mkdir("./log/")
+
     logger.debug("Start of __main__")
 
-    channel = "#bottest"
-    bot = FruityBot(channel)
-    logger.debug("Bot instantiated")
-    bot.start()
+    # create factory protocol and application
+    f = BotFactory("bottest")
+
+    # connect factory to this host and port
+    try:
+        reactor.connectTCP(utils.Config("config.json").config.main.server, 6667, f)
+    except FileNotFoundError:
+        reactor.connectTCP(utils.Config("config.json.template").config.main.server, 6667, f)
+
+    # run bot
+    reactor.run()
 
 
 if __name__ == "__main__":
